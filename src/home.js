17a586b6710a3bb815b7372651ee8f238e367f6d
DzianisPilipuk <dzianis.pilipuk@gmail.com>
2023-05-20 21:50:39 +0200

    Add map to home page

--- src/home.js
+++ src/home.js
@@ -20,10 +20,24 @@ const appendInvitationPageContent = (parent) => {
 
   const placeOrderButton = document.createElement("button");
   placeOrderButton.textContent = "Place your order";
-
   invitationPageContent.appendChild(placeOrderButton);
 
   addContactInfo(invitationPageContent);
+
+  const map = document.createElement("iframe");
+  map.setAttribute(
+    "src",
+    `https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2434.148996455816!2d16.9307709!3d52.40397
+      58!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47045b394c4ad563%3A0xc3e60cb15a05878d!2zUMO
+      zxYJ3aWVqc2thIDExLCA2MS04NTAgUG96bmHFhA!5e0!3m2!1sen!2spl!4v1684611715818!5m2!1sen!2spl`
+  );
+  map.setAttribute("width", "400");
+  map.setAttribute("height", "300");
+  map.setAttribute("style", "border:0");
+  map.setAttribute("allowfullscreen", "");
+  map.setAttribute("loading", "lazy");
+  map.setAttribute("referrerpolicy", "no-referrer-when-downgrade");
+  invitationPageContent.appendChild(map);
 };
 
 export default appendInvitationPageContent;
