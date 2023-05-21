621c7dff3bf7fb066f406506633637f73279b980
DzianisPilipuk <dzianis.pilipuk@gmail.com>
2023-05-21 21:29:25 +0200

    Improve contact info segment

--- src/addContactInfo.js
+++ src/addContactInfo.js
@@ -1,21 +1,42 @@
 import createDiv from "./createDiv";
 
 const addContactInfo = (parent) => {
-  const telephone = createDiv("telephone");
-  parent.appendChild(telephone);
+  const numberWrapper = createDiv("number-wrapper");
+  parent.appendChild(numberWrapper);
 
-  const numberWrapper = createDiv("number-wrapper", "555-555-555");
+  const phoneIcon = document.createElement("img");
+  phoneIcon.setAttribute("src", "./assets/phone-solid.svg");
+  phoneIcon.setAttribute("alt", "phone");
+  numberWrapper.appendChild(phoneIcon);
 
-  telephone.appendChild(numberWrapper);
+  const phoneNumber = createDiv("phone-number", "555-555-555");
+  numberWrapper.appendChild(phoneNumber);
 
-  const workingHours = createDiv(
-    "working-hours",
-    "MON - FRI 7am - 5pm SAT 10am - 3pm"
+  const workingHoursWrapper = createDiv("working-hours-wrapper");
+  parent.appendChild(workingHoursWrapper);
+
+  const workingHoursMonFri = createDiv(
+    "working-hours-wrapper-mon-fri",
+    "MON - FRI 7am - 5pm"
   );
-  parent.appendChild(workingHours);
+  workingHoursWrapper.appendChild(workingHoursMonFri);
+
+  const workingHoursSat = createDiv(
+    "working-hours-wrapper-sat",
+    "SAT 10am - 3pm"
+  );
+  workingHoursWrapper.appendChild(workingHoursSat);
+
+  const locationWrapper = createDiv("location-wrapper");
+  parent.appendChild(locationWrapper);
+
+  const locationIcon = document.createElement("img");
+  locationIcon.setAttribute("src", "./assets/location-dot-solid.svg");
+  locationIcon.setAttribute("alt", "location");
+  locationWrapper.appendChild(locationIcon);
 
   const location = createDiv("location", "Półwiejska 11 61-850 Poznań");
-  parent.appendChild(location);
+  locationWrapper.appendChild(location);
 };
 
 export default addContactInfo;
